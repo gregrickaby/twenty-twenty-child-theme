81c5c9c25cbe2c27fb6c80b42d2ddc4dc212600e
Greg Rickaby <greg@gregrickaby.com>
2020-08-07 11:14:26 -0500

    add web core vitals

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './index.scss';
 import Prism from 'prismjs';
+import { getCLS, getFID, getLCP } from 'web-vitals';
+getCLS( console.log, true );
+getLCP( console.log, true );
+getFID( console.log ); // Does not accept a `true` parameter.
