92d9782303e065e9f12e9116ac4c89fdb4c58ace
Greg Rickaby <gregrickaby@gmail.com>
2021-04-13 14:17:57 -0500

    use asset file

--- functions.php
+++ functions.php
@@ -16,14 +16,18 @@
  * @since 1.0.0
  */
 function twentytwentychild_enqueue() {
+
+	// Include asset file.
+	$asset_file = include 'build/index.asset.php';
+
 	// Enqueue parent theme style.
-	wp_enqueue_style( 'twentytwenty', get_template_directory_uri() . '/style.css', null, wp_get_theme()->parent()->get( 'Version' ) );
+	wp_enqueue_style( 'twentytwenty', get_template_directory_uri() . '/style.css', null, $asset_file['version'] );
 
 	// Enqueue child theme style.
-	wp_enqueue_style( 'twentytwenty-child-style', get_stylesheet_directory_uri() . '/build/index.css', array( 'twentytwenty' ), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'twentytwenty-child-style', get_stylesheet_directory_uri() . '/build/index.css', [], $asset_file['version'] );
 
 	// Enqueue child theme script.
-	wp_enqueue_script( 'twentytwenty-child-script', get_stylesheet_directory_uri() . '/build/index.js', array( 'twentytwenty-js' ), wp_get_theme()->get( 'Version' ), true );
+	wp_enqueue_script( 'twentytwenty-child-script', get_stylesheet_directory_uri() . '/build/index.js', $asset_file['dependencies'], $asset_file['version'], true );
 	wp_script_add_data( 'twentytwenty-child-script', 'async', true );
 }
 add_action( 'wp_enqueue_scripts', 'twentytwentychild_enqueue' );
