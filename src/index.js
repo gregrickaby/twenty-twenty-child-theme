7867677cc71e66852e12773b32ab078a4a5847b7
Greg Rickaby <gregrickaby@gmail.com>
2021-04-15 20:38:55 -0500

    bump

--- src/index.js
+++ src/index.js
@@ -1,3 +1,117 @@
 import './index.css';
 // Uncomment to use Prism.
 //import Prism from 'prismjs';
+
+wp.blocks.registerBlockVariation( 'core/columns', {
+	name: 'unicorn',
+	title: 'Unicorn',
+	isDefault: true,
+	attributes: {
+		className: 'unicorn-columns',
+	},
+	icon: 'buddicons-activity',
+	scope: [ 'inserter' ],
+	innerBlocks: [
+		[
+			'core/column',
+			{},
+			[
+				[
+					'core/heading',
+					{
+						level: 3,
+						content: 'Column 1 Heading',
+					},
+				],
+				[
+					'core/paragraph',
+					{
+						content:
+							'Lorem ipsum dolor sit amet id erat aliquet diam ullamcorper tempus massa eleifend vivamus.',
+					},
+				],
+			],
+		],
+		[
+			'core/column',
+			{},
+			[
+				[
+					'core/heading',
+					{
+						level: 3,
+						content: 'Column 2 Heading',
+					},
+				],
+				[
+					'core/paragraph',
+					{
+						content:
+							'Morbi augue cursus quam pulvinar eget volutpat suspendisse dictumst mattis id.',
+					},
+				],
+			],
+		],
+	],
+} );
+
+wp.blocks.registerBlockVariation( 'core/cover', {
+	name: 'dogs',
+	title: 'Dogs',
+	isDefault: true,
+	attributes: {
+		className: 'dog-hero',
+		align: 'full',
+		contentAlign: 'left',
+		backgroundType: 'image',
+		url: 'https://jcnextjs.local/wp-content/uploads/2021/04/hero.jpg',
+	},
+	icon: 'carrot',
+	innerBlocks: [
+		[
+			'core/heading',
+			{
+				level: 1,
+				content: 'They’re good dogs, Brent',
+			},
+		],
+		[
+			'core/paragraph',
+			{
+				content:
+					'The only Legit Source for Professional Dog Ratings on the web.',
+			},
+		],
+		[
+			'core/button',
+			{
+				text: 'Get your rating',
+				backgroundColor: 'vivid-cyan-blue',
+				textColor: 'white',
+				url: '#',
+			},
+		],
+		[
+			'core/button',
+			{
+				text: 'Dispute a rating',
+				className: 'is-style-outline',
+				textColor: 'white',
+				url: '#',
+			},
+		],
+	],
+} );
+
+wp.blocks.registerBlockVariation( 'core/heading', {
+	name: 'green-text',
+	title: 'Green Text',
+	description: 'This block has green text.',
+	attributes: {
+		content: 'Green Text',
+		level: 1,
+		textColor: 'vivid-green-cyan',
+	},
+	icon: 'palmtree',
+	scope: [ 'inserter' ],
+} );
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
+const MiniCSSExtractPlugin = require( 'mini-css-extract-plugin' );
+const ESLintPlugin = require( 'eslint-webpack-plugin' );
+const StylelintPlugin = require( 'stylelint-webpack-plugin' );
+
+/**
+ * Webpack config (Development mode)
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-scripts/#provide-your-own-webpack-config
+ */
+module.exports = {
+	...defaultConfig,
+	module: {
+		rules: [
+			{
+				test: /\.(sa|sc|c)ss$/,
+				exclude: '/node_modules',
+				use: [
+					MiniCSSExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.svg$/,
+				use: [ '@svgr/webpack', 'url-loader' ],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'fonts/[name].[hash:8].[ext]',
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		...defaultConfig.plugins,
+
+		/**
+		 * Clean build directory.
+		 *
+		 * @see https://www.npmjs.com/package/clean-webpack-plugin
+		 */
+		new CleanWebpackPlugin(),
+
+		/**
+		 * Report JS warnings and errors to the command line.
+		 *
+		 * @see https://www.npmjs.com/package/eslint-webpack-plugin
+		 */
+		new ESLintPlugin(),
+
+		/**
+		 * Report CSS warnings and errors to the command line.
+		 *
+		 * @see https://www.npmjs.com/package/stylelint-webpack-plugin
+		 */
+		new StylelintPlugin(),
+	],
+};
